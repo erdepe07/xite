bcda528375fef9278f7af4516fd1e58db02ac799
erfanhuda <erfan_huda@rocketmail.com>
2025-11-18 10:51:08 +0700

    add more

--- aciraba_website/public/app/v1/datatable/extension/scroller.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(function(){$("#datatable-1").DataTable({ajax:"https://blueupcodes.com/assets/datatables/api.json",deferRender:true,scrollCollapse:true,scrollY:300,scroller:true})});
--- aciraba_website/public/scripts/hakakses.js
+++
@@ -1,8 +0,0 @@
-/*let JSONHakakses = JSON.parse(jsonMenu);
-$(document).ready(function() {
-    panggilHakakses();
-})
-function panggilHakakses(){
-    console.log(JSONHakakses.menuakses[1].status)
-    if (JSONHakakses.menuakses[1].menuke == "ha_daftaritem" && JSONHakakses.menuakses[1].status == "0") $("#menu_daftaritem").css("display",none);
-}*/
