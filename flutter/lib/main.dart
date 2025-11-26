990cf2883da2720f98e4a6d57c8407c53b8513cd
erfanhuda <me@erfanhuda.com>
2025-11-26 16:34:31 +0700

    change back main dart using login screen first

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,30 +1,30 @@
 // The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
 
-// // The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
-//
-// // import 'package:flutter/material.dart';
-// // import 'login_screen.dart'; // Import halaman login
-// //
-// // void main() {
-// //   runApp(const MyApp());
-// // }
-// //
-// // class MyApp extends StatelessWidget {
-// //   const MyApp({super.key});
-// //
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return MaterialApp(
-// //       title: 'Aplikasi Ramin',
-// //       theme: ThemeData(
-// //         primarySwatch: Colors.blue,
-// //       ),
-// //       // Set LoginoScreen sebagai halaman utama
-// //       home: const LoginScreen(),
-// //     );
-// //   }
-// // }
-//
+import 'package:flutter/material.dart';
+import 'login_screen.dart'; // Import halaman login
+import 'package:flut/src/rust/api/simple.dart';
+import 'package:flut/src/rust/frb_generated.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplikasi Ramin',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // Set LoginoScreen sebagai halaman utama
+      home: const LoginScreen(),
+    );
+  }
+}
+
 // import 'package:flutter/material.dart';
 // import 'package:flut/src/rust/api/simple.dart';
 // import 'package:flut/src/rust/frb_generated.dart';
@@ -44,37 +44,10 @@
 //         appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
 //         body: Center(
 //           child: Text(
-//               'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+//             'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
+//           ),
 //         ),
 //       ),
 //     );
 //   }
 // }
-//
-
-import 'package:flutter/material.dart';
-import 'package:flut/src/rust/api/simple.dart';
-import 'package:flut/src/rust/frb_generated.dart';
-
-Future<void> main() async {
-  await RustLib.init();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
-          ),
-        ),
-      ),
-    );
-  }
-}
